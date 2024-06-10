0420dc61b40a5ce632b73ca93e4f1234c82c5b15
Guilherme Ranoya <ranoya@gmail.com>
2024-06-10 18:56:56 -0300

    R2 e P

---
+++ p/research/index.php
@@ -0,0 +1,61 @@
+<?php
+$where = $_SERVER['HTTP_ACCEPT_LANGUAGE']." ";
+$lan = substr($where, 0, 2);
+
+if ($lan == "pt") { 
+
+header('Location: https://www.ranoya.com/public/pesquisa');
+
+} else {
+
+header('Location: https://www.ranoya.com/publicinternational/pesquisa');
+
+}
+?>
+
+<script type="text/javascript">
+
+   /* -- Google Analytics ---------------------------------------------------- */ 
+
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-61225449-1', 'auto');
+  ga('send', 'pageview');
+
+   /* ---------------------------------------------------- Google Analytics -- */
+
+</script>
+
+
+<!DOCTYPE html>
+<html>
+  
+  <head>
+    <meta charset="utf-8"/>
+    <title>Guilherme Ranoya, PhD</title>
+    <meta name="generator" content="artesanal" />
+    <meta name="description" content="Website pessoal do Professor Dr. Guilherme Ranoya. Descrição, notas e referências sobre os projetos sendo desenvolvidos atualmente; referências em tecnologia, criatividade, experiência de usário, computação física, computação vestível, e outros contúdos.">
+    <meta name="keywords" content="UX, User Experience, IA, Arquitetura de Informação, IxD, Design de Interação, Mídia Digital, Wearable Computing, Physical Computing, Pesquisa">
+    <meta name="author" content="Guilherme Ranoya">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <link REL="SHORTCUT ICON" HREF="https://www.ranoya.com/favicon.ico"> 
+    <link rel="image_src" type="image/jpg" href="https://profile.ranoya.com/cover.jpg" />
+	  <meta property="og:image" content="https://profile.ranoya.com/cover.jpg" />
+	  <meta property="og:image:width" content="1145" />
+	  <meta property="og:image:height" content="688" />
+    <link href='https://www.ranoya.com/public/style/form.css' rel='stylesheet' type='text/css'>
+    <!-- Chrome, Firefox OS and Opera -->
+    <meta name="theme-color" content="#620384">
+    <meta name="msapplication-navbutton-color" content="#620384">
+    <meta name="apple-mobile-web-app-status-bar-style" content="#620384">
+    <meta name = "pinterest" content = "nopin" />
+    <meta name="pinterest" content="nohover"> 
+ 
+  </head>
+  
+</html>
+
+
