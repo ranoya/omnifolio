d94622bce7b7c29137436c28fe2c6b7b64a662ab
Guilherme Ranoya <ranoya@gmail.com>
2026-05-06 11:04:21 -0300

    tentando outra solução

---
+++ next.config.js
@@ -0,0 +1,13 @@
+// next.config.js
+module.exports = {
+  images: {
+    domains: [
+      "vercel.app",
+      "ranoya.com",
+      "ufpe.vercel.app",
+      "omniboards.vercel.app",
+      "slidelines.vercel.app",
+      "booklines.vercel.app",
+    ],
+  },
+};
